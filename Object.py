8751f12dc624dfd53fab2d4c26d7bf775a5f031c
MikeSE314 <micheal.erickson3.14@gmail.com>
2015-01-31 16:12:11 -0700

    update

--- Object.py
+++ Object.py
@@ -19,14 +19,18 @@ class SpaceObject(object):
         self.zOn = False
         self.power = 10.5
         self.makeSomethingAwesome(objLoader.OBJ(file))
+
     def draw(self):
         self.drawer.draw_obj((self.q.w, self.q.x, self.q.y, self.q.z), self.Ps, self.obj.gl_list)
+
     def makeSomethingAwesome(self, obj):
         self.obj = obj
+
     def update(self):
         self.kp = self.keys.get_keys()
         self.Vs = self.Vs.add(self.As.div(30.)).mult(self.dampner)
         self.Ps = self.Ps.add(self.Vs.div(30.))
         self.As, self.dampner = Vector(0, 0, 0), 1.0
+
 if __name__ == "__main__":
     print(dir(pygame.locals))
